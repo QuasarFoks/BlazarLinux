82f640f23431b105f04ad400d61c6c03ec8d3e35
B-e-n-z1342 <QuasarFoks@proton.me>
2026-02-27 14:46:42 +0300

    сборка

--- build.sh
+++ build.sh
@@ -11,4 +11,19 @@ echo "//        3) chroot и отвязка от host                     //"
 echo "//                                                        //"
 echo "////////////////////////////////////////////////////////////"
 #clear
-
+read -p "[1-3]>> " choise
+case "$choise" in
+    1)
+        export BLAZAR=/mnt/blazar
+        export TOOL="$BLAZAR"/tools
+        export SOURCES="$BLAZAR"/sources
+        build/build_tool.sh
+        ;;
+    2)
+        export BLAZAR=/mnt/blazar
+        export TOOL="$BLAZAR"/tools
+        export SOURCES="$BLAZAR"/sources
+        build/crossbuild.sh
+        ;;
+    3) echo "Реализуется" ;;
+esac
